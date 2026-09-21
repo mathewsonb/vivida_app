cdf1e3acb99e74e2fbac31c689bae0640c6040c6
Ben <mathewsonb@plu.edu>
2026-09-21 01:14:53 -0700

    have tested on local machine and debugged for successful proof of concept

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   // Sets '/' for local dev ('npm run dev') and '/vivida/' only when building ('npm run build')
-  base: command === 'serve' ? '/' : '/vivida/',
+  base: process.env.NODE_ENV === 'production' ? '/vivida/' : '/',
   server: {
     port: 5173,
     open: true,
